d57029a3bd3a62053aad345cae030e8ebc0048b4
Шмаков Артем Александрович <a.shmakov@dcoa.ru>
2023-03-25 21:53:56 +0300

    update server to new api

--- internal/server/server_test.go
+++ internal/server/server_test.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"bytes"
 	"io"
 	"net/http"
 	"net/http/httptest"
@@ -9,8 +10,9 @@ import (
 	"github.com/go-chi/chi/v5"
 )
 
-func testRequest(t *testing.T, ts *httptest.Server, method, path string) (int, string) {
-	req, _ := http.NewRequest(method, ts.URL+path, nil)
+func testRequest(t *testing.T, ts *httptest.Server, method, path string, body []byte) (int, string) {
+	req, _ := http.NewRequest(method, ts.URL+path, bytes.NewBuffer(body))
+	req.Header.Set("Content-Type", "application/json")
 
 	resp, _ := http.DefaultClient.Do(req)
 	respBody, _ := io.ReadAll(resp.Body)
@@ -36,119 +38,91 @@ func TestChiRouter(t *testing.T) {
 	s.chiRouter.Route("/", func(router chi.Router) {
 		s.chiRouter.Get("/", s.listHandler)
 		s.chiRouter.Post("/", s.defaultHandler)
-		s.chiRouter.Get("/value/{metricType}/{metricName}", s.GetValueHandler)
-		s.chiRouter.Post("/update/{metricType}/{metricName}/{metricValue}", s.UpdateHandler)
+		s.chiRouter.Post("/value/", s.GetValueHandler)
+		s.chiRouter.Post("/update/", s.UpdateHandler)
 	})
+
+	var reqBody []byte
+	var body string
+	var statusCode int
 
 	ts := httptest.NewServer(s.chiRouter)
 	defer ts.Close()
 
-	statusCode, _ := testRequest(t, ts, "GET", "/")
+	statusCode, _ = testRequest(t, ts, "GET", "/", nil)
 	if statusCode != http.StatusOK {
 		t.Errorf("Error")
 	}
 
-	statusCode, _ = testRequest(t, ts, "POST", "/")
+	statusCode, _ = testRequest(t, ts, "POST", "/", nil)
 	if statusCode != http.StatusForbidden {
 		t.Errorf("Error")
 	}
 
-	statusCode, _ = testRequest(t, ts, "POST", "/update/counter/PollCount/1")
+	// test update gauge
+	reqBody = []byte("{\"id\":\"Alloc\",\"type\":\"gauge\",\"value\":100}")
+	statusCode, _ = testRequest(t, ts, "POST", "/update/", reqBody)
 	if statusCode != http.StatusOK {
 		t.Errorf("Error")
 	}
 
-	statusCode, _ = testRequest(t, ts, "POST", "/update/counter/PollCount/2")
+	// test get gauge
+	reqBody = []byte("{\"id\":\"Alloc\",\"type\":\"gauge\"}")
+	statusCode, body = testRequest(t, ts, "POST", "/value/", reqBody)
+	if statusCode != http.StatusOK || body != "100" {
+		t.Errorf("Error")
+	}
+
+	// test insupported type
+	reqBody = []byte("{\"id\":\"Alloc\",\"type\":\"counter\"}")
+	statusCode, _ = testRequest(t, ts, "POST", "/value/", reqBody)
+	if statusCode != http.StatusNotFound {
+		t.Errorf("Error")
+	}
+
+	// test update counter
+	reqBody = []byte("{\"id\":\"PollCount\",\"type\":\"counter\",\"delta\":1}")
+	statusCode, _ = testRequest(t, ts, "POST", "/update/", reqBody)
 	if statusCode != http.StatusOK {
 		t.Errorf("Error")
 	}
 
-	statusCode, body := testRequest(t, ts, "GET", "/value/counter/PollCount")
-	if statusCode != http.StatusOK || body != "3" {
+	// test get counter
+	reqBody = []byte("{\"id\":\"PollCount\",\"type\":\"counter\"}")
+	statusCode, body = testRequest(t, ts, "POST", "/value/", reqBody)
+	if statusCode != http.StatusOK || body != "1" {
 		t.Errorf("Error")
 	}
-}
 
-func TestGetMethod(t *testing.T) {
-	serv := NewServer()
-
-	wr := httptest.NewRecorder()
-
-	req := httptest.NewRequest(http.MethodGet, "/1", nil)
-	serv.UpdateHandler(wr, req)
-	if wr.Code == http.StatusOK {
-		t.Errorf("Method Get not allowed")
+	// test insupported type
+	reqBody = []byte("{\"id\":\"PollCount\",\"type\":\"gauge\"}")
+	statusCode, _ = testRequest(t, ts, "POST", "/value/", reqBody)
+	if statusCode != http.StatusNotFound {
+		t.Errorf("Error")
 	}
 
-	req = httptest.NewRequest(http.MethodGet, "/update/", nil)
-	serv.UpdateHandler(wr, req)
-	if wr.Code == http.StatusOK {
-		t.Errorf("Method Get not allowed")
+	// === test invalid counter ===
+	reqBody = []byte("{\"id\":\"PollCount\",\"type\":\"counter\"}")
+	statusCode, _ = testRequest(t, ts, "POST", "/update/", reqBody)
+	if statusCode != http.StatusForbidden {
+		t.Errorf("Error")
 	}
 
-	req = httptest.NewRequest(http.MethodGet, "/update/gauge/Alloc/1.00", nil)
-	serv.UpdateHandler(wr, req)
-	if wr.Code == http.StatusOK {
-		t.Errorf("Method Get not allowed")
-	}
-}
-
-func TestPostMethod(t *testing.T) {
-	serv := NewServer()
-
-	wr := httptest.NewRecorder()
-
-	req := httptest.NewRequest(http.MethodPost, "/1", nil)
-	serv.UpdateHandler(wr, req)
-	if wr.Code == http.StatusOK {
-		t.Errorf("got HTTP status code %d, expected 200", wr.Code)
+	reqBody = []byte("{\"id\":\"PollCount\",\"type\":\"counter111\"}")
+	statusCode, _ = testRequest(t, ts, "POST", "/update/", reqBody)
+	if statusCode != http.StatusForbidden {
+		t.Errorf("Error")
 	}
 
-	req = httptest.NewRequest(http.MethodPost, "/update/", nil)
-	serv.UpdateHandler(wr, req)
-	if wr.Code == http.StatusOK {
-		t.Errorf("got HTTP status code %d, expected 200", wr.Code)
+	reqBody = []byte("{}")
+	statusCode, _ = testRequest(t, ts, "POST", "/update/", reqBody)
+	if statusCode != http.StatusForbidden {
+		t.Errorf("Error")
 	}
 
-	req = httptest.NewRequest(http.MethodPost, "/update/gauge", nil)
-	serv.UpdateHandler(wr, req)
-	if wr.Code == http.StatusOK {
-		t.Errorf("got HTTP status code %d, expected 200", wr.Code)
-	}
-
-	req = httptest.NewRequest(http.MethodPost, "/update/gauge/Alloc/1.00", nil)
-	serv.UpdateHandler(wr, req)
-	if wr.Code == http.StatusOK {
-		t.Errorf("got HTTP status code %d, expected 200", wr.Code)
-	}
-}
-
-func TestUpdate(t *testing.T) {
-	serv := NewServer()
-
-	wr := httptest.NewRecorder()
-
-	req := httptest.NewRequest(http.MethodPost, "/update/gauge/Alloc/1.00", nil)
-	serv.UpdateHandler(wr, req)
-	if wr.Code != http.StatusOK {
-		t.Errorf("status code != 200")
-	}
-
-	req = httptest.NewRequest(http.MethodPost, "/update/gauge/Alloc/2.00", nil)
-	serv.UpdateHandler(wr, req)
-	if wr.Code != http.StatusOK {
-		t.Errorf("status code != 200")
-	}
-
-	req = httptest.NewRequest(http.MethodPost, "/update/counter/PollCount/1", nil)
-	serv.UpdateHandler(wr, req)
-	if wr.Code != http.StatusOK {
-		t.Errorf("status code != 200")
-	}
-
-	req = httptest.NewRequest(http.MethodPost, "/update/counter/PollCount/2", nil)
-	serv.UpdateHandler(wr, req)
-	if wr.Code != http.StatusOK {
-		t.Errorf("status code != 200")
+	reqBody = []byte("{\"id\":\"\",\"type\":\"counter\",\"delta\":1}")
+	statusCode, _ = testRequest(t, ts, "POST", "/update/", reqBody)
+	if statusCode != http.StatusForbidden {
+		t.Errorf("Error")
 	}
 }
